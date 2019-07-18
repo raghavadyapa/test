e3125bbdaa977c8d3a7370a81a37b5793b30f5c2
raghavadyapa <50195544+raghavadyapa@users.noreply.github.com>
2019-07-18 12:24:50 +0530

    Create simpleRead.java

---
+++ src/main/java/com/techolution/ipcybris/simpleRead.java
@@ -0,0 +1,23 @@
+package com.techolution.ipcybris;
+
+import org.apache.beam.runners.dataflow.DataflowPipelineRegistrar;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.runners.dataflow.TestDataflowPipelineOptions;
+
+public class simpleRead {
+
+  public static void main(String args[])
+  {
+    PipelineOptions options=PipelineOptionsFactory.create();
+    Pipeline p=Pipeline.create(options);
+
+    p.apply(TextIO.read().from("gs://uspto_data/text-1.txt"))
+                        .apply(TextIO.write().to("gs://uspto_data/text-2.txt"));
+  }
+
+
+}
